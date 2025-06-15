e3418008a745c287707f5d10fbdd9dced91447b7
Aryannaik-max <naikaryan949@gmail.com>
2025-06-15 18:02:55 +0530

    generated required models, created crud repository and city-service

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-
-const bodyParser = require('body-parser');
 const express = require('express');
-const { PORT } = require('./config/serverConfig');
+const bodyParser = require('body-parser');
+
+const { PORT, DB_SYNC } = require('./config/serverConfig');
+const db = require('./models/index');
+
 const app = express();
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+
+
 app.listen(PORT,()=>{
     console.log(`server connected at PORT: ${PORT}`);
+    if(DB_SYNC){
+        db.sequelize.sync({alter:true});
+    }
 })
---
+++ src/models/airplain.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Airplain extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Airplain.init({
+    modelNumber: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    capacity: {
+      type: DataTypes.INTEGER,
+      allowNull:false
+    }
+  }, {
+    sequelize,
+    modelName: 'Airplain',
+  });
+  return Airplain;
+};
